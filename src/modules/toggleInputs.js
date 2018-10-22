e156e89aa740d4868a7af310bbffe84f1d801b8d
Aron Jon Prenovost <Aroniascoder@gmail.com>
2018-10-22 07:18:23 -0500

    updated time format

--- src/modules/toggleInputs.js
+++ src/modules/toggleInputs.js
@@ -6,11 +6,22 @@ let manualModeButton = document.getElementById('manual'),
 	datepicker = document.getElementById('datepicker');
 	// dateEntryToggle = document.getElementById('entry-date-toggle');
 
+// convert time to 12 hour AM/PM format
+const formatAMPM = (date) => { 
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+	let strTime = hours + ':' + minutes + ' ' + ampm;
+	return strTime;
+}
+
 const currentTimeManualInput = () => { 
-    let d = new Date();
-    let n = d.toLocaleTimeString().replace(/:\d{2}\s/,' ');
-    manualInput1.value = n;
-	manualInput2.value = n;
+	let date = new Date(); 
+	manualInput1.value = formatAMPM(date); 
+	manualInput2.value = formatAMPM(date);
 }
 
 // jquery datepicker 
