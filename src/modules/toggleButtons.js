1fd16d215aea20e4bd9c9bebaee5a14ac13582db
Aron Jon Prenovost <Aroniascoder@gmail.com>
2019-03-06 22:26:36 -0800

    resume time button

--- src/modules/toggleButtons.js
+++ src/modules/toggleButtons.js
@@ -11,12 +11,30 @@ const toggleBilling = () => {
 const resumeTime = () => {
 	document.addEventListener('click', function (event) {
 		if (event.target.classList.contains('fa-play')) {
-		// add flag class 
-		event.target.classList.toggle('billed');
-		const billIcons = [...document.getElementsByClassName('fa-play')];
-			for (var x = 0; x < billIcons.length;x++) {
-				console.log(billIcons[x]);
+		event.target.classList.add('act-play'); // add flag class 
+		const playIcons = [...document.getElementsByClassName('fa-play')];
+			for (let x = 0; x < playIcons.length;x++) {
+				if (playIcons[x].classList.contains('act-play')) {
+				let index = x; 
+				console.log(index);
+
+				if (index === 0) {
+        			return false; 
+    			}
+
+    			//let item = listEntries[index];
+
+			    // append task 
+			  //  document.getElementById('placeholder').value = item.task_name;
+
+			    // append project name 
+			  //  projToggle.innerHTML = 'Project - ' + item.project_name;
+
+			  // startButton.click();
+
+				}
 			}
+			event.target.classList.remove('act-play');
 		}
 	}, false);
 }
